a820ffa5196118788857aaafb2951f90c066f81e
pm3512 <aobolens@andrew.cmu.edu>
2022-12-19 11:26:06 -0500

    improved rust setup script

--- rust_setup/main.rs
+++ rust_setup/main.rs
@@ -1,7 +1,14 @@
+use std::{env, fs};
+
 mod part1;
 mod part2;
 
 fn main() {
-    part1::solve();
-    part2::solve();
+    let args: Vec<String> = env::args().collect();
+    let default_filename = String::from("inputs/in1.txt");
+    let in_filename = &args.get(1).unwrap_or(&default_filename);
+
+    let input = fs::read_to_string(in_filename).unwrap();
+    part1::solve(&input);
+    part2::solve(&input);
 }
--- rust_setup/part.rs
+++ rust_setup/part.rs
@@ -1,8 +1,3 @@
-use std::env;
-use std::fs;
-
-pub fn solve() {
-    let args: Vec<String> = env::args().collect();
-    let default_filename = String::from("inputs/in1.txt");
-    let in_filename = &args.get(1).unwrap_or(&default_filename);
+#[allow(dead_code)]
+pub fn solve(input: &String) {
 }
